7c4b992721e9dd26062f5b9ae6b952bb76a68868
lakshminarayana100 <113232213+lakshminarayana100@users.noreply.github.com>
2022-09-12 18:55:46 +1400

    Add files via upload

---
+++ patternc..cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int m,n;
+	printf("enter the number of printed:");
+	scanf("%d",n);
+	printf("enter the max number of time printed:");
+	scanf("%d",m);
+	
+	return 0;
+}
